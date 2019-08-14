8fc1b4fe2bef96af2a73f12a2ae65c1900a8f668
David Blevins <david.blevins@gmail.com>
2019-08-14 00:17:10 -0700

    Utils.sh to hold reusable functions
    Signed-off-by: David Blevins <david.blevins@gmail.com>

---
+++ promotion/utils.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+################################
+# 
+# Script of reusable functions
+#
+# Keeps the main scripts cleaner
+# and cuts down on duplication
+#
+################################
+
+function fail {
+    echo "$1"
+    exit 1
+}
+
+function require {
+    local name="${1?Specify a variable name}"
+    local -n value="$name"
+    local regex="$2"
+    
+    # Fail if the variable name doesn't exist
+    [ -n "$value" ] || fail "$name environment variable was not set"
+
+    # (optional) Match against a regex and fail if it doesn't match
+    [ -n "$regex" ] && {
+	echo "$value" | egrep --silent "^$regex$" ||
+	    fail "$name value \"$value\" does not match pattern \"$regex\""
+    }
+}
