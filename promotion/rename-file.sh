b5498b16c3c2c5a1ed4fcbbb3a00ae90644aeddc
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2025-04-04 09:51:21 +0200

    remove tck stuff

--- promotion/rename-file.sh
+++ promotion/rename-file.sh
@@ -51,55 +51,12 @@ UPDATED_KEYRING=/tmp/updated
     gpg --armor --export 'jakarta.ee-spec@eclipse.org' > "$WORKSPACE/jakartaee-spec-committee.updated.pub" || fail "Unable to export jakartaee-spec-committee.updated.pub from $GNUPGHOME"
 )
 
-( # Create a tmp dir and download the TCK
-    TMP="/tmp/download-$$" && mkdir "$TMP" && cd "$TMP"
-
-    for url in $FILE_URLS; do
-        # Local file name of the TCK zip or tar.gz
-        file="$(basename "$url")"
-
-        # File name must start with safe characters
-        # No passing us a "-r" or ".." file or some cleverness
-        require file "[a-zA-Z0-9].*"
-
-        # Download the file or fail
-        curl -L "$url" > "$file" || fail "Could not download $url"
-
-        # If the tck starts with "eclipse-" rename it "jakarta-"
-        [[ "$file" == eclipse-* ]] && cp "$file" "${file/eclipse-/jakarta-}"
-
-    done
-
-    # Our list of files before we start signing.  Used in the final report step
-    FILES=($(ls))
-
-    ( # Hash and sign
-        for file in *; do
-            ( # Sign using the committee keyring
-                export GNUPGHOME="$COMMITTEE_KEYRING"
-                passphrase_tmp="$(mktemp)"
-                echo "$PASSPHRASE" > "$passphrase_tmp"
-                gpg --batch --yes --pinentry-mode loopback --passphrase-file "$passphrase_tmp" --output "$file".sig --detach-sig "$file" || fail "Signature failed"
-            )
-
-            ( # Verify using the consumer keyring
-                export GNUPGHOME="$CONSUMER_KEYRING"
-
-                # If someone has rotated the private key but NOT updated the
-                # public key file in github, this step will fail
-                gpg --verify "$file".sig "$file" || fail "Outdated Public Keys. Signature Verification failed for \"$file\". Our keys have likely been rotated and we need to publish jakartaee-spec-committee.updated.pub"
-            )
-
-            # Calculate the sha256 for convenience
-            shasum -a 256 "$file" | tr ' ' '\t' | cut -f 1 > "$file.sha256" || fail "SHA-256 creation failed for \"$file\""
-        done
-    )
 
     ZONE="/home/data/httpd/download.eclipse.org/jakartaee/"
     HOST='genie.jakartaee-spec-committee@projects-storage.eclipse.org'
 
-ssh "$HOST" "ls -la $ZONE" || echo "Remote directory missing \"$ZONE\""
-ssh "$HOST" "ls -la $SRC" || echo "Src file missing \"$SRC\""
+    ssh "$HOST" "ls -la $ZONE" || echo "Remote directory missing \"$ZONE\""
+    ssh "$HOST" "ls -la $SRC" || echo "Src file missing \"$SRC\""
 
-echo "Would cp ${SRC} to ${DEST}..."
-ssh "$HOST" "cd $ZONE; cp $SRC $DEST; cp ${SRC}.sha256 ${DEST}.sha256; cp ${SRC}.sig ${DEST}.sig;" || echo "Failed to cp file \"$SRC\""
+    echo "Would cp ${SRC} to ${DEST}..."
+    ssh "$HOST" "cd $ZONE; cp $SRC $DEST; cp ${SRC}.sha256 ${DEST}.sha256; cp ${SRC}.sig ${DEST}.sig;" || echo "Failed to cp file \"$SRC\""
